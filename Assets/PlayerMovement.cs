7ffe97ec8ac33d31c3faeb0ef2526753404e4893
Nicholas Andrada <nandrada@csumb.edu>
2019-03-12 13:49:48 -0700

    Fungus stuff

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class PlayerMovement : MonoBehaviour
 {
 	private Rigidbody rb;
 	public float speed;
+	bool hasSword = true;
+	public Flowchart flowchart;
+	public GameObject gameObject;
     // Start is called before the first frame update
     void Start()
     {
 		rb = GetComponent<Rigidbody>();
+		gameObject = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
@@ -18,7 +23,15 @@ public class PlayerMovement : MonoBehaviour
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
+		updateStatus();
 		rb.AddForce(movement * speed);
+	}
+
+	void updateStatus()
+	{
+		if (hasSword == true)
+		{
+			flowchart.hasSword = true;
+		}
 	}
 }
